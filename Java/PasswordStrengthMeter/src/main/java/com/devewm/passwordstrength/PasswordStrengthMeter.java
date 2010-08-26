4781531682b9b6dc92c243cb033fb9fe4ccdc11f
Eric Montgomery <ericwmontgomery@gmail.com>
2010-08-25 22:26:12 -0400

    updated algorithm, should now be correct for inputs that do not fall outside the specified character range

--- Java/PasswordStrengthMeter/src/main/java/com/devewm/passwordstrength/PasswordStrengthMeter.java
+++ Java/PasswordStrengthMeter/src/main/java/com/devewm/passwordstrength/PasswordStrengthMeter.java
@@ -1,5 +1,13 @@
 package com.devewm.passwordstrength;
 
+import java.text.DecimalFormat;
+
+/**
+ * Algorithms:
+ * every possible combination: 3^3 + 3^2 + 3^1
+ * @author eric
+ *
+ */
 public class PasswordStrengthMeter {
 	public static final long check(String passwordPlaintext) {
 		return PasswordStrengthMeter.check(passwordPlaintext, null);
@@ -30,11 +38,67 @@ public class PasswordStrengthMeter {
 		// determine number of iterations required for brute force attack
 		// within this character range
 		long result = 0;
-		result += (long) Math.pow(highestCharCode - lowestCharCode, passwordPlaintext.length() - 1);
 		
-		int lastChar = passwordPlaintext.charAt(passwordPlaintext.length() - 1);
-		result += Math.abs(lastChar - lowestCharCode);
+		long rangeSize = highestCharCode - lowestCharCode + 1;
+		for(int i = 1; i < passwordPlaintext.length(); i++) {
+			result += Math.pow(rangeSize, i);
+		}
 		
-		return result;
+		for(int i = 1; i <= passwordPlaintext.length(); i++) {
+			int power = passwordPlaintext.length() - i;
+			int placeValue = passwordPlaintext.charAt(i - 1) - lowestCharCode;
+			
+			if(power == 0 && placeValue == 0) {
+				continue;
+			}
+			// 8 = 8 * 10^0, 80 = 8 * 10^1
+			result += (placeValue * Math.pow(rangeSize, power)); 
+		}
+		
+		return ++result;
+	}
+	
+	public static void main(String[] args) {
+		if(args.length < 1) {
+			printUsage();
+			return;
+		}
+		PasswordCharacterRange range = PasswordCharacterRange.MINIMAL_FOR_INPUT;
+		int passwordStartIndex = 0;
+		
+		if(args[0].substring(0,1).equals("-")) {
+			if(args.length < 2) {
+				printUsage();
+				return;
+			}
+			passwordStartIndex = 1;
+			range = PasswordCharacterRange.valueOf(args[0].substring(1));
+		}
+		
+		StringBuffer password = new StringBuffer();
+		for(int i = passwordStartIndex; i < args.length; i++) {
+			password.append(args[i] + " ");
+		}
+		password.setLength(password.length() - 1);
+		
+		DecimalFormat number = new DecimalFormat();
+		number.setGroupingUsed(true);
+		long result = PasswordStrengthMeter.check(password.toString(), range);
+		System.out.println(password + ": " + number.format(result));
+	}
+	
+	private static void printUsage() {
+		String className = PasswordStrengthMeter.class.getName();
+		System.out.println();
+		System.out.println(className);
+		System.out.println("http://devewm.com/projects/passwordstrength\n");
+		System.out.println("Usage:");
+		System.out.println("   java " + className + " [-<character range type>] <password>");
+		System.out.println("example:\n   java " + className + " -ALPHABET_MIXED_CASE chickeN\n");
+		System.out.println("Possible values for character range type:");
+		for(PasswordCharacterRange range : PasswordCharacterRange.values()) {
+			System.out.println("\t-" + range.name());
+		}
+		System.out.println();
 	}
 }
