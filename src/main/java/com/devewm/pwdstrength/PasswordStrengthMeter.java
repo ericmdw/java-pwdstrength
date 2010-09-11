2e3e7a7e2ab4e1c7411dd46fd5e7e889eab6c8cf
Eric Montgomery <ericwmontgomery@gmail.com>
2010-09-11 16:22:02 -0400

    further documentation on PasswordStrengthMeter

--- src/main/java/com/devewm/pwdstrength/PasswordStrengthMeter.java
+++ src/main/java/com/devewm/pwdstrength/PasswordStrengthMeter.java
@@ -14,19 +14,37 @@ import com.devewm.pwdstrength.exception.UnsupportedImplementationException;
  * <p>The range of characters checked is determined by scanning the password's characters to see what unicode character blocks they fall in. All the occuring blocks will then be scanned during the simulated brute-force attack. For instance, if a password contains a single character in the cyrillic unicode block, all characters in that block will be checked when running the attack iteration count. (The Basic_Latin block has been subdivided into lowercase letters, uppercase letters, numbers, symbols, and control characters.)</p>
  * <p>With this approach, the method for counting the sequential brute-force iterations can be expressed mathematically as:
  * given <em>R</em> is the number of characters which could occur in the password; <em>L</em> is the length of the password; <em>P[n]</em> is the 0-based index of the <em>n</em>th character of the password in the range of possible characters; then<br/>
- * iterations = <em>(R^1 + R^2 + ... + R^(L-1)) + { P[0](R^(L-0)) + P[1](R^(L-1)) + ... + P[L-1](R^(L-L)) }</em>
+ * iterations = <em>(R^1 + R^2 + ... + R^(L-1)) + { P[0](R^(L-0)) + P[1](R^(L-1)) + ... + P[L-1](R^(L-L)) + 1}</em>
  * </p>
  * <p>This class cannot be instantiated; to use it, get an instance of the class by calling the static <code>getInstance()</code> method. Subclasses should override <code>getInstance()</code> to provide a singleton instance of the appropriate type. The <code>getInstance(Class type)</code> method is provided as a convenience to access singleton instances of subtypes when applicable.</p>
  *
  */
 public class PasswordStrengthMeter {
+	/**
+	 * The maximum password length (in unicode codepoints) for which
+	 * calculations will be run. To bypass this limit, use
+	 * <code>iterationCount()</code> and pass <code>true</code> as the
+	 * <code>bypassLengthLimitCheck</code> parameter.
+	 */
 	public static final int PASSWORD_LENGTH_LIMIT = 256;
 	
+	/**
+	 * Implementation instance cache to assist this and subclasses with
+	 * singleton usage.
+	 */
 	private static Map<Class<? extends PasswordStrengthMeter>, Object> impls;
 	
+	/**
+	 * Reducing constructor visibility to enforce singleton usage. To
+	 * get an instance of this class, call <code>getInstance()</code>.
+	 */
 	protected PasswordStrengthMeter() {
 	}
 	
+	/**
+	 * Get an instance of <code>PasswordStrengthMeter</code>
+	 * @return an instance of <code>PasswordStrengthMeter</code>
+	 */
 	public static PasswordStrengthMeter getInstance() {
 		if(null == impls) {
 			impls = new HashMap<Class<? extends PasswordStrengthMeter>, Object>();
@@ -40,6 +58,16 @@ public class PasswordStrengthMeter {
 		return (PasswordStrengthMeter) impl;
 	}
 	
+	/**
+	 * Get the singleton instance of a <code>PasswordStrengthMeter</code>
+	 * subclass. This is here as a convenience to custom factory-type
+	 * classes so they don't have to manage the caching of the subclass
+	 * implementations.
+	 * @param clazz the <code>PasswordStrengthMeter</code> subclass to return
+	 * @return singleton instance of the given subclass
+	 * @throws UnsupportedImplementationException if the subclass type could
+	 * not be found or instantiated
+	 */
 	public static PasswordStrengthMeter getInstance(Class<? extends PasswordStrengthMeter> clazz) {
 		if(null == impls) {
 			impls = new HashMap<Class<? extends PasswordStrengthMeter>, Object>();
@@ -57,6 +85,20 @@ public class PasswordStrengthMeter {
 		return (PasswordStrengthMeter) impl;
 	}
 	
+	/**
+	 * Check to see if the given password satisfies the given
+	 * <code>PasswordStrengthClass</code>. This is determined by running
+	 * <code>iterationCount</code> on the given password and comparing it
+	 * to the pre-computed minimum iteration count of the given
+	 * <code>PasswordStrengthClass</code>. Note that if the password
+	 * length exceeds <code>PASSWORD_LENGTH_LIMIT</code>, no check
+	 * will be run and this method will automatically return <code>true</code>.
+	 * @param password the password to check
+	 * @param strengthClass the strength class to compare to
+	 * @return <code>true</code> if the password satisfies the strength class (or
+	 * the password length exceeds <code>PASSWORD_LENGTH_LIMIT</code>),
+	 * <code>false</code> otherwise.
+	 */
 	public boolean satisfiesStrengthClass(String password, PasswordStrengthClass strengthClass) {
 		BigInteger iterationCount = null;
 		try {
@@ -70,10 +112,31 @@ public class PasswordStrengthMeter {
 		return iterationCount.compareTo(strengthClass.getIterations()) >= 0;
 	}
 	
+	/**
+	 * Get the number of brute-force iterations needed to arrive at the given
+	 * password. See the class description for the algorithm used to determine
+	 * the count.
+	 * @param passwordPlaintext the password to calculate the count for
+	 * @return a <code>BigInteger</code> representing the number of iterations
+	 * needed to arrive at the given password
+	 */
 	public BigInteger iterationCount(String passwordPlaintext) {
 		return iterationCount(passwordPlaintext, false);
 	}
 	
+	/**
+	 * Get the number of brute-force iterations needed to arrive at the given
+	 * password. See the class description for the algorithm used to determine
+	 * the count.
+	 * @param passwordPlaintext the password to calculate the count for
+	 * @param bypassLengthLimitCheck true to ignore the hard-coded
+	 * 
+	 * @return a <code>BigInteger</code> representing the number of iterations
+	 * @throws MaximumPasswordLengthExceededException if 
+	 * <code>bypassLengthLimitCheck</code> is <code>true</code> and the
+	 * length of <code>passwordPlaintext</code> exceeds
+	 * <code>PASSWORD_LENGTH_LIMIT</code>
+	 */
 	public BigInteger iterationCount(String passwordPlaintext, boolean bypassLengthLimitCheck) {
 		if(null == passwordPlaintext || passwordPlaintext.length() < 1) {
 			return new BigInteger("0");
@@ -111,6 +174,10 @@ public class PasswordStrengthMeter {
 		return result.add(new BigInteger("1"));
 	}
 	
+	/**
+	 * main method for running this utility from the command line.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		if(args.length < 1) {
 			printUsage();
@@ -130,6 +197,9 @@ public class PasswordStrengthMeter {
 		System.out.println(password + ": " + number.format(result));
 	}
 	
+	/**
+	 * Usage instructions for running this utility from the command line.
+	 */
 	private static void printUsage() {
 		String className = PasswordStrengthMeter.class.getName();
 		System.out.println();
